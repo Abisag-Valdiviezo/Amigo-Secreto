5a298753afcf75558aa0472d38958bbde6678443
Abisag Valdiviezo <abisag.valdiviezo79@gmail.com>
2025-02-19 14:04:39 -0600

    Recorre el array con un bucle for

--- app.js
+++ app.js
@@ -14,16 +14,24 @@ function agregarAmigo() {
         //console.log(amigos); 
 
     limpiarCampo();
-    elementoLista();
-
+    actualizarLista();
+        
     }  
     return;
 
 }
 
-function elementoLista(){
-    let lista = document.getElementById('amigo');
+function actualizarLista(){
+    let lista = document.getElementById("listaAmigos");
     lista.innerHTML ="";
+
+    for(let i=0; i <amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+
+
  }
 
 function limpiarCampo(){
